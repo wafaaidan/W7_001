c944fd802ed5cf7aa618b6b6b16ae524efdaaecb
wafaaidan <wafa.aidan@gmail.com>
2026-05-13 09:48:11 +0700

    constructor elektronik

--- Elektronik.java
+++ Elektronik.java
@@ -1,5 +1,8 @@
 public class Elektronik extends Produk{
     private int garansi;
 
-    
+    public Elektronik(String nama, double harga, int stok, int garansi){
+        super(nama, harga, stok);
+        this.garansi = garansi;
+    }
 }
