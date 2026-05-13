c635b25e436a485ba3b121aaeabad04d2232ae70
wafaaidan <wafa.aidan@gmail.com>
2026-05-13 09:39:32 +0700

    deklarasi variable & membuat constructor

--- Produk.java
+++ Produk.java
@@ -1,3 +1,11 @@
 public class Produk {
-    
+    private String nama;
+    private double harga;
+    private int stok;
+
+    public Produk(String nama, double harga, int stok) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
 }
