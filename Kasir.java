a63c35bab3fb67186eb0690653802c0287f18f9a
wafaaidan <wafa.aidan@gmail.com>
2026-05-13 10:07:05 +0700

    membuat kondisi  stok

--- Kasir.java
+++ Kasir.java
@@ -9,4 +9,16 @@ public class Kasir {
         System.out.println("=== TOKO KITA ===");
         System.out.println("Nama Kasir: " + namaKasir);
     }
+
+    public void prosesBelanja(Produk p, int jmlBeli) {
+
+        if (p.getStok() < jmlBeli) {
+            System.out.println("Out of Stock");
+        } else {
+            double hargaAkhir = p.hitungHargaAkhir(jmlBeli);
+            System.out.println("Harga Akhir = " + hargaAkhir);
+            p.setStok(jmlBeli);
+        }
+        System.out.println("Stok terbaru = " + p.getStok());
+    }
 }
