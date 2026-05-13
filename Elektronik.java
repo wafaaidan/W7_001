00fda8a54108ca2bbf9ad7d55b46fe5d0c47fc51
wafaaidan <wafa.aidan@gmail.com>
2026-05-13 09:52:22 +0700

    membuat override

--- Elektronik.java
+++ Elektronik.java
@@ -5,4 +5,14 @@ public class Elektronik extends Produk{
         super(nama, harga, stok);
         this.garansi = garansi;
     }
+    @Override
+    public double hitungHargaAkhir(int jmlBeli) {
+        System.out.println("Total Harga = " + hitungTotalHarga(jmlBeli));
+        return hitungTotalHarga(jmlBeli) * 0.85;
+    }
+    @Override 
+    public void tampilInfo(){
+        super.tampilInfo();
+        System.out.println("Masa Garansi: " + garansi + "Tahun");
+    }
 }
