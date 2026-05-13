112669a861047e7df9483cd93d64f049be9d8e5a
wafaaidan <wafa.aidan@gmail.com>
2026-05-13 10:12:11 +0700

    upcasting

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Produk p1 = new Elektronik("Polytron", 500000, 6, 5);
-    
+        Produk p2 = new Makanan("Mie Ayam", 8000, 100, 2);
+        Kasir kasir = new Kasir("Giorgio");
+
+        
     }
 }
