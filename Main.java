ed43cc4d69a3f5369207f126a8d2b7a9f0b3b3b9
wafaaidan <wafa.aidan@gmail.com>
2026-05-13 10:14:42 +0700

    membuat output

--- Main.java
+++ Main.java
@@ -4,6 +4,12 @@ public class Main {
         Produk p2 = new Makanan("Mie Ayam", 8000, 100, 2);
         Kasir kasir = new Kasir("Giorgio");
 
-        
+        p1.tampilInfo();
+        kasir.tampilIdentitas();
+        kasir.prosesBelanja(p1, 3);
+
+        p2.tampilInfo();
+        kasir.tampilIdentitas();
+        kasir.prosesBelanja(p2, 11);
     }
 }
