b25ae680b920e076852b7b0f8ba137da77be872a
wafaaidan <wafa.aidan@gmail.com>
2026-05-17 21:39:32 +0700

    membuat tabel data produk array

--- Main.java
+++ Main.java
@@ -1,15 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Produk p1 = new Elektronik("Polytron", 500000, 6, 5);
-        Produk p2 = new Makanan("Mie Ayam", 8000, 100, 2);
+        Produk[] database = {
+            new Elektronik("Laptop", 10000000, 5, 2),
+            new Makanan("Snack", 10000, 100, 30),
+            new Elektronik("Smartphone", 5000000, 10, 1)
+        };
+        // Produk p1 = new Elektronik("Polytron", 500000, 6, 5);
+        // Produk p2 = new Makanan("Mie Ayam", 8000, 100, 2);
         Kasir kasir = new Kasir("Giorgio");
+        
+        // p1.tampilInfo();
+        // kasir.tampilIdentitas();
+        // kasir.prosesBelanja(p1, 3);
 
-        p1.tampilInfo();
-        kasir.tampilIdentitas();
-        kasir.prosesBelanja(p1, 3);
-
-        p2.tampilInfo();
-        kasir.tampilIdentitas();
-        kasir.prosesBelanja(p2, 11);
+        // p2.tampilInfo();
+        // kasir.tampilIdentitas();
+        // kasir.prosesBelanja(p2, 11);
     }
 }
